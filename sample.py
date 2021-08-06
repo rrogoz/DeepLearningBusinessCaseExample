f3d24796e60cde4e533fb1d76dbc6e22816fd0ae
rrogoz <87890809+rrogoz@users.noreply.github.com>
2021-08-06 13:31:06 +0200

    update

--- sample.py
+++ sample.py
@@ -19,8 +19,7 @@ test_targets = npz['targets'].astype(np.int)
 
 # model
 
-input_size = 10
-output_size = 2
+output_size = 1
 hidden_layer_size = 50
 early_stooping = tf.keras.callbacks.EarlyStopping(patience=2)
 # no_hidden_layers = 2
@@ -30,13 +29,15 @@ model = tf.keras.Sequential([
                             tf.keras.layers.Dense(
                                 hidden_layer_size, activation='relu'),
                             tf.keras.layers.Dense(
-                                output_size, activation='softmax')
+                                hidden_layer_size, activation='relu'),
+                            tf.keras.layers.Dense(
+                                output_size, activation='sigmoid')
 
                             ])
 custom_optimizer = tf.keras.optimizers.Adam(
     learning_rate=0.0001)   # optimizer settings
 model.compile(optimizer=custom_optimizer,
-              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+              loss='binary_crossentropy', metrics=['accuracy'])
 # prepere batch
 
 BATCH_SIZE = 20
